84043d5340dd9770b9f2c3a94a819d1288e847f4
Pavol_P <pavol.panisz@gmail.com>
2024-05-10 21:05:40 +0200

    implemented feedback from WED May 8th

--- vue2/js/363.20f27fb7.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkproject1"]=self["webpackChunkproject1"]||[]).push([[363],{8363:function(e,n,r){function t(e,n){return" (wip) Create a new shopping list "}r.r(n),r.d(n,{default:function(){return s}});var u=r(8493);const c={},p=(0,u.A)(c,[["render",t]]);var s=p}}]);
-//# sourceMappingURL=363.20f27fb7.js.map
